c4043794e36b6afbfefcfbda4f8dc8afb92109dc
Lalithaditya007 <179290841+Lalithaditya007@users.noreply.github.com>
2026-09-20 18:19:51 +0000

    Solve: Transformation Matrix from Basis B to C

---
+++ problems/0027-transformation-matrix-from-basis-b-to-c/solution.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def transform_basis(B: list[list[int]], C: list[list[int]]) -> list[list[float]]:
+    B = np.array(B, dtype=float)
+    C = np.array(C, dtype=float)
+
+    P = np.linalg.inv(C) @ B
+    return P.tolist()
